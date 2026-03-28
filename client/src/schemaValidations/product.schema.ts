deced66f9772c71283a7b3f848a090c4c05926a1
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2026-03-28 09:47:05 +0700

    update client

--- client/src/schemaValidations/product.schema.ts
+++ client/src/schemaValidations/product.schema.ts
@@ -1,14 +1,13 @@
-import { z } from "zod";
+import z from 'zod'
 
 export const CreateProductBody = z.object({
   name: z.string().min(1).max(256),
-  // Đổi từ z.coerce.number().positive() -> z.number().positive()
-  price: z.number().positive(),
+  price: z.coerce.number().positive(),
   description: z.string().max(10000),
-  image: z.string().url(),
-});
+  image: z.string().url()
+})
 
-export type CreateProductBodyType = z.infer<typeof CreateProductBody>;
+export type CreateProductBodyType = z.TypeOf<typeof CreateProductBody>
 
 export const ProductSchema = z.object({
   id: z.number(),
@@ -17,27 +16,26 @@ export const ProductSchema = z.object({
   description: z.string(),
   image: z.string(),
   createdAt: z.date(),
-  updatedAt: z.date(),
-});
-
-export type ProductResType = z.infer<typeof ProductRes>;
+  updatedAt: z.date()
+})
 
 export const ProductRes = z.object({
   data: ProductSchema,
-  message: z.string(),
-});
+  message: z.string()
+})
+
+export type ProductResType = z.TypeOf<typeof ProductRes>
 
 export const ProductListRes = z.object({
   data: z.array(ProductSchema),
-  message: z.string(),
-});
+  message: z.string()
+})
 
-export type ProductListResType = z.infer<typeof ProductListRes>;
+export type ProductListResType = z.TypeOf<typeof ProductListRes>
 
-export const UpdateProductBody = CreateProductBody;
-export type UpdateProductBodyType = z.infer<typeof UpdateProductBody>;
-
+export const UpdateProductBody = CreateProductBody
+export type UpdateProductBodyType = CreateProductBodyType
 export const ProductParams = z.object({
-  id: z.coerce.number(),
-});
-export type ProductParamsType = z.infer<typeof ProductParams>;
+  id: z.coerce.number()
+})
+export type ProductParamsType = z.TypeOf<typeof ProductParams>
